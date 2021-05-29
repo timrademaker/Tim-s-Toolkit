50020bf77b3feabf89eb18930f1cfabf140569f7
timrademaker <10331650+timrademaker@users.noreply.github.com>
2021-05-29 22:16:16 +0200

    Hid IsFeatureEnabled node as it's no longer needed by users

--- TimsToolkit/Source/FeatureFlags/Public/FeatureFlagFunctionLibrary.h
+++ TimsToolkit/Source/FeatureFlags/Public/FeatureFlagFunctionLibrary.h
@@ -23,6 +23,6 @@ public:
      * @param FeatureName The name of the feature
      * @return True if the feature is enabled
      */
-	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (Category = "Tim's Toolkit|Feature Flags", AutoCreateRefTerm = "FeatureName"))
+	UFUNCTION(BlueprintCallable, BlueprintPure, meta = (Category = "Tim's Toolkit|Feature Flags", AutoCreateRefTerm = "FeatureName", BlueprintInternalUseOnly = "true"))
     static bool IsFeatureEnabled(const FName& FeatureName);
 };
